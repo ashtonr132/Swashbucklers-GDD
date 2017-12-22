e202ba554ff9e18583a7fe5bf3819ea29e0ffd16
ashtonr132 <ashtonr132@gmail.com>
2017-12-22 23:37:11 +0000

    Button functionality added, you can now traverse scenes

--- Assets/ButtonHandler.cs
+++ Assets/ButtonHandler.cs
@@ -7,16 +7,43 @@ using UnityEngine.UI;
 public class ButtonHandler : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-        GameObject.Find("Canvas/Play").GetComponent<Button>().onClick.AddListener(ButtonFunc);
+	void Start ()
+    {
+        if (SceneManager.GetActiveScene().name == "Main Menu")
+        {
+            GameObject.Find("Canvas/Play").GetComponent<Button>().onClick.AddListener(delegate { LoadManageScene("Shipyard"); });
+            // GameObject.Find("Canvas/Settings").GetComponent<Button>().onClick.AddListener(delegate { LoadManageScene("Settings"); });
+            GameObject.Find("Canvas/Exit").GetComponent<Button>().onClick.AddListener(ExitGame);
+        }
+        if (SceneManager.GetActiveScene().name == "Market")
+        {
+            GameObject.Find("Canvas/Save and Quit").GetComponent<Button>().onClick.AddListener(delegate { LoadManageScene("Main Menu"); });
+            GameObject.Find("Canvas/Docks").GetComponent<Button>().onClick.AddListener(delegate { LoadManageScene("Shipyard"); });
+            GameObject.Find("Canvas/Start Combat").GetComponent<Button>().onClick.AddListener(delegate { LoadManageScene("Naval Combat"); });
+        }
+        if (SceneManager.GetActiveScene().name == "Naval Combat")
+        {
+
+        }
+        if (SceneManager.GetActiveScene().name == "Shipyard")
+        {
+            GameObject.Find("Canvas/Save and Quit").GetComponent<Button>().onClick.AddListener(delegate { LoadManageScene("Main Menu"); });
+            GameObject.Find("Canvas/Market").GetComponent<Button>().onClick.AddListener(delegate { LoadManageScene("Market"); });
+            GameObject.Find("Canvas/Start Combat").GetComponent<Button>().onClick.AddListener(delegate { LoadManageScene("Naval Combat"); });
+        }
     }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
-    void ButtonFunc()
+    void LoadManageScene(string scenename)
     {
-        SceneManager.LoadScene("Management");
+        SceneManager.LoadScene(scenename);
+    }
+    void ExitGame()
+    {
+
+        Application.Quit();
     }
 }
