7a701fd58d70858e13ed4107a06cf99e60b2ac3b
ashtonr132 <ashtonr132@gmail.com>
2017-12-22 17:08:16 +0000

    scene setups, play button scripted

---
+++ Assets/ButtonHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject.Find("Canvas/Play").GetComponent<Button>().onClick.AddListener(ButtonFunc);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void ButtonFunc()
+    {
+        SceneManager.LoadScene("Management");
+    }
+}
